5d7b53d2a5ae1838e139d5a42d86f4d89ca7daf2
Ahmadjonov-98 <akhmadjonovr.98@gmail.com>
2023-05-06 17:58:48 +0500

    task4 model

---
+++ apps/task4/models.py
@@ -0,0 +1,14 @@
+from datetime import timedelta
+import random
+
+from django.utils import timezone
+from django.db.models import Model, DateTimeField, ForeignKey, CASCADE, IntegerField
+
+from apps.task1.models import User
+
+
+class Activate(Model):
+    user = ForeignKey(User,CASCADE, 'codes')
+    start_date = DateTimeField(auto_now=True)
+    expire_date = DateTimeField(timezone.now()+timedelta(minutes=2))
+    code = IntegerField(default=random.randint())
